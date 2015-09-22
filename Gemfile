d7b904126e9b7403f0379325154f8c55d82c1ab8
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-09-22 09:12:39 -0700

    add simplecov

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem "simple_form"
 
 gem "faker"
 
+gem 'simplecov', require: false, group: :test
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -22,9 +22,19 @@ RSpec.feature "Sessions", type: :feature do
     end
 
     context "unsuccesful login" do
-      it "stays on the same and see a message saying can't login and dont see
+      it "stays on the same and see a message saying Wrong Credentials! and dont see
           you're name on the page" do
+          visit new_session_path
 
+          # in this case it will look for a input field with id email
+          fill_in "Email", with: user.email
+          fill_in "Password", with: user.email
+
+          click_button "Sign In"
+
+          expect(current_path).to eq(sessions_path)
+          expect(page).to have_text /Wrong Credentials!/i
+          expect(page).not_to have_text /#{user.full_name}/i
       end
     end
   end
